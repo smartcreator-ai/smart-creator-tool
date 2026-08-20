f0684b8d1c47bb169e0af48af211646329bf1519
smartcreator-ai <aimovie2008@gmail.com>
2026-08-20 16:36:40 +0630

    Update script-writer.js

--- frontend/script-writer.js
+++ frontend/script-writer.js
@@ -46,7 +46,7 @@ generateButton.addEventListener("click", async () => {
 
 
         const response = await fetch(
-            "http://localhost:3000/generate-script",
+            ""https://smart-creator-backend-production.up.railway.app/generate-script",
             {
 
                 method:"POST",
